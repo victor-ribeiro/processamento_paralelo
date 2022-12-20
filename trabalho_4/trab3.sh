aca2e5ede9d29355d7714fea37fb716aac204dc0
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2022-12-20 10:32:47 -0300

    MPI

---
+++ trabalho_4/trab3.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+gcc -O3 -fopt-info-optimized-missed=vect_info.report -fopenmp transpose_openmp.c -o transpose.out
+
+threads=2
+rep=3
+tam=35000
+tile=2500
+
+echo "gerando diferentes impleplementacoes"
+for i in 1 2 3;
+do
+    ./transpose.out $i $threads $rep $tam $tile;
+done
+
+
+echo "tile fixo"
+for i in 2 4 8 16
+do
+    ./transpose.out 3 $i $rep $tam $tile;
+done
